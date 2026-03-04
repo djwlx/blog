3f2c44b897f6687438c4e94da47eacfee039ae7e
DJWL <58622651+djwlx@users.noreply.github.com>
2026-03-04 09:03:26 +0000

    feat: require

--- source/js/waline.js
+++ source/js/waline.js
@@ -3,9 +3,11 @@ Waline.init({
   serverURL: 'https://waline.djwl.top',
   path: window.location.pathname,
   meta: ['nick', 'mail'],
+  requiredMeta: ['mail'],
   lang: 'zh-CN',
   dark: 'html[data-user-color-scheme="dark"]',
   pageSize: 10,
+  noRss: true,
   emoji: [
     '//unpkg.com/@waline/emojis@1.4.0/tieba',
     '//unpkg.com/@waline/emojis@1.4.0/bilibili',
